2c1c44791e8249972fa2bb9899cdf3c4cf5d7f75
tostos <saittosun2@gmail.com>
2020-08-10 09:58:22 +0200

    event delegation

--- chapter-1/sandbox.js
+++ chapter-1/sandbox.js
@@ -11,17 +11,21 @@ button.addEventListener('click', () => {
   // ul.prepend(li);
 });
 
-const items = document.querySelectorAll('li');
-// console.log(items);
-items.forEach(item => {
-  // console.log(item);
-  item.addEventListener('click', (e) => {
-    console.log('event in LI');
-    e.stopPropagation(); 
-    e.target.remove();
-  });
-});
+// const items = document.querySelectorAll('li');
+// // console.log(items);
+// items.forEach(item => {
+//   // console.log(item);
+//   item.addEventListener('click', (e) => {
+//     console.log('event in LI');
+//     e.stopPropagation(); 
+//     e.target.remove();
+//   });
+// });
 
-ul.addEventListener('click', () => {
-  console.log('event in UL');
+ul.addEventListener('click', (e) => {
+  // console.log('event in UL');
+  console.log(e.target);// li de event listener yok ama bubling den dolayi parent teki event listener i okuyor ve console da hangi li yi tikladi isek onu gosteriyor. bu ayni zamanda button daki click event de etkisini gosterecek. because we are not attaching the event listener to the li's anymore so it doesn't matter that new these li tags don't have event listeners. What matters is that we're clicking them and the event bubbles up to the well which we do have an event   listener attached to.
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
 });
